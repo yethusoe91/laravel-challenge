64b6f41344cb7217c17c1bd42e3f24be33f24f5d
Ye Thu Soe <mr.yethusoe@gmail.com>
2023-03-18 02:09:51 +0630

    Task 3

--- database/migrations/2022_01_26_091312_create_tags_table.php
+++ database/migrations/2022_01_26_091312_create_tags_table.php
@@ -15,7 +15,7 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->string('value');
+            $table->string('name');
             $table->timestamps();
         });
     }
